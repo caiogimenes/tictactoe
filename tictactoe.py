135f66b7abedd3e6d7e08765e9f9f46ecefb0cda
caio <ciaonemegi@gmail.com>
2024-04-25 18:15:05 -0300

    main

--- tictactoe.py
+++ tictactoe.py
@@ -22,29 +22,49 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
-
+    count_x = 0
+    count_o = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == X:
+                count_x+=1
+            elif board[i][j] == O:
+                count_o+=1
+    
+    return 'X' if count_x <= count_o else 'O'
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
-
+    actions = []
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                actions.append((i,j))
+        
+    return actions
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    board_temp = board.deepcopy()
+    board_temp[action[0]][action[1]] == player(board)
 
+    return board_temp
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
-
+    """     
+    central = board[1][1]
+    
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == central:    
+                
+    return
 
 def terminal(board):
     """
